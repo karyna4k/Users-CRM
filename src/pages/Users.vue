97a5d0f6b9196f3e9de7e1a7a4f14aed89d3ead9
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-08-25 22:23:08 +0300

    feat: add table of users

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -2,8 +2,39 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <h1>Users page</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Gender</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in users" :key="user.id">
+              <td>{{ user.name }}</td>
+              <td>{{ user.age }}</td>
+              <td>{{ user.gender }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    this.users = [
+      { id: 1, name: "Jack", age: 22, gender: "male" },
+      { id: 2, name: "Alex", age: 24, gender: "male" },
+    ];
+  },
+};
+</script>
