26af25799d171ccf6f7cef55aee7e3d9847e2459
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-08-26 16:13:12 +0300

    feat: prepare to deploy

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   assetsDir: '',
   productionSourceMap: false,
-  filenameHashing: true
-}
+  filenameHashing: true,
+};
